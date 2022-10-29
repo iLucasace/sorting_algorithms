44ea9f0594a4c45f1244ad96739af93c34f1cc36
Lucas Eckhardt <lucas.ace@hotmail.com>
2022-10-28 22:49:33 -0300

    merge sort fixed

--- merge_sort.c
+++ merge_sort.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 
-void MERGESORT(char* V, int left, int right);
-void MERGE(char* V, int left1, int right1, int left2, int right2);
+void MERGESORT(char* V, int l, int r);
+void MERGE(char* V, int l1, int r1, int l2, int r2);
 void SHOW_VET(char *V, int length);
 
 void main() {
@@ -14,46 +14,46 @@ void main() {
 }
 
 
-void MERGESORT(char* V, int left, int right) {
-    if (left < right) {
-        int middle = (left + right) / 2;
-        MERGESORT(V, left, middle);
-        MERGESORT(V, middle + 1, right);
-        MERGE(V, left, middle, middle + 1, right);
+void MERGESORT(char* V, int l, int r) {
+    if (l < r) {
+        int m = (l + r) / 2;
+        MERGESORT(V, l, m);
+        MERGESORT(V, m + 1, r);
+        MERGE(V, l, m, m + 1, r);
     }
 }
 
-void MERGE(char* V, int left1, int right1, int left2, int right2) {
-    int aux_array[right2 - left1 + 1];
-    int i = left1;
-    int j = left2;
+void MERGE(char* V, int l1, int r1, int l2, int r2) {
+    int aux[r2 - l1 + 1];
+    int i = l1;
+    int j = l2;
     int k = 0;
 
-    while (i <= right1 && j <= right2) {
+    while (i <= r1 && j <= r2) {
         if (V[i] < V[j]) {
-            aux_array[k] = V[i];
+            aux[k] = V[i];
             i++;
         } else {
-            aux_array[k] = V[j];
+            aux[k] = V[j];
             j++;
         }
         k++;
     }
 
-    while (i <= right1) {
-        aux_array[k] = V[i];
+    while (i <= r1) {
+        aux[k] = V[i];
         i++;
         k++;
     }
 
-    while (j <= right2) {
-        aux_array[k] = V[j];
+    while (j <= r2) {
+        aux[k] = V[j];
         j++;
         k++;
     }
 
-    for (i = left1, j = 0; i <= right2; i++, j++) {
-        V[i] = aux_array[j];
+    for (i = l1, j = 0; i <= r2; i++, j++) {
+        V[i] = aux[j];
     }
 }
 
